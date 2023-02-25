d5e7e903d9a4494fea3ee67067fd9cfa23c6d525
engrab <engrbilawalhussain@gmail.com>
2023-02-25 10:45:19 +0500

    init

--- app/src/main/java/com/example/calculatordemo/MainActivity.kt
+++ app/src/main/java/com/example/calculatordemo/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.calculatordemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.calculatordemo.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.cos
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,12 +17,15 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener{ calculateTip() }
     }
 
-    private fun calculateTip() {
+     private fun calculateTip() {
 
         val costStr = binding.costOfService?.text.toString()
-        val cost = costStr.toDouble()
-        val buttonId = binding.tipOptions.checkedRadioButtonId
-        val percent = when (buttonId) {
+        val cost = costStr.toDoubleOrNull()
+         if (cost == null || cost == 0.0){
+             displayTip(0.0)
+             return
+         }
+         val percent = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -28,11 +33,16 @@ class MainActivity : AppCompatActivity() {
         }
         var result = cost * percent
 
-        val checked = binding.roundUpSwitch.isChecked
-        if (checked){
+
+        if (binding.roundUpSwitch.isChecked){
             result = kotlin.math.ceil(result)
         }
 
-        binding.tipResult.text= result.toString()
+        displayTip(result)
+    }
+
+    private fun displayTip(tip : Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
